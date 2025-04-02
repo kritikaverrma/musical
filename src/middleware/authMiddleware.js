dbb171a86dadaef832cc17cb54584b8ed1bd0e09
Kritika Verma <kritikaverma51510@gmail.com>
2025-04-02 17:35:56 +0530

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,8 +4,19 @@ import { getAuth } from "@clerk/express";
 
 export const protectRoute = async (req, res, next) => {
     console.log("req at protectRoute", req.rawHeaders);
-    const auth = getAuth(req); // Extract auth manually
-    console.log("Manual Auth:", auth);
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+    return res.status(401).json({ message: "No token found in headers" });
+  }
+
+  try {
+    const verifiedToken = await clerkClient.verifyToken(token);
+    console.log("✅ Verified Token:", verifiedToken);
+    res.json({ verifiedToken });
+  } catch (error) {
+    console.error("❌ Token verification failed:", error);
+    res.status(401).json({ message: "Invalid or expired token", error });
+  }
     if (!auth.userId) {
         return res.status(401).json({ message: "Unauthorized - you must be logged in" });
     }
