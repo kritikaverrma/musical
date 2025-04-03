7346ff782824baedc10ecb09483192f3e17e8732
Kritika Verma <kritikaverma51510@gmail.com>
2025-04-03 19:29:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -45,13 +45,13 @@ app.use(
 	})
 );
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin); // Dynamic origin
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
-});*/
+});
 
 
 app.options("*", cors()); // Handle pre-flight requests globally
